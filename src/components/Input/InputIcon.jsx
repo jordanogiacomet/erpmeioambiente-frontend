3be077c8078c6e030a2cf7c5a65158f02f9e17e1
Jordano <jgtomazoni@ucs.br>
2024-07-30 12:11:06 -0300

    WIP - Composition pattern implementado;

--- src/components/Input/InputIcon.jsx
+++ src/components/Input/InputIcon.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import P from 'prop-types';
 import { Icon } from './styles/Icon';
 
-export const InputIcon = ({ icon: IconComponent, ...props }) => {
-  return <Icon icon={IconComponent} {...props}/>;
+export const InputIcon = ({ icon }) => {
+  return <Icon icon={icon} />;
 };
 
 InputIcon.propTypes = {
-  icon: P.elementType.isRequired,
+  icon: P.oneOfType([P.object, P.array]).isRequired,
 };
